30265a31815834b19ffcabee6fbd04009dacd928
Amir M-Aghaei <amir.maghaei@gmail.com>
2019-08-09 12:08:37 +0200

    Make spin variables constant

--- src/spin_definitions.jl
+++ src/spin_definitions.jl
@@ -1,12 +1,12 @@
 eye(n) = sparse(1.0I, n, n)
 
 # generic spin_half
-sz_half = sparse(Float64[.5 0; 0 -.5])
-sp_half = sparse(Float64[ 0 1; 0  0])
-sm_half = sparse(Float64[ 0 0; 1  0])
-I2 = eye(2)
+const sz_half = sparse(Float64[.5 0; 0 -.5])
+const sp_half = sparse(Float64[ 0 1; 0  0])
+const sm_half = sparse(Float64[ 0 0; 1  0])
+const I2 = eye(2)
 
 # U1 symmetric spin_half
-sz_half_U1sym = SymTensor(0, (STLeg(+1, [0, 1], [1, 1]), STLeg(-1, [0, 1], [1, 1])),
+const sz_half_U1sym = SymTensor(0, (STLeg(+1, [0, 1], [1, 1]), STLeg(-1, [0, 1], [1, 1])),
                           [(0, 0), (1, 1)],
                           [-.5*ones(1,1), .5*ones(1,1)])
