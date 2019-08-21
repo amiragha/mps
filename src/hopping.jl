7d27aaa5c772b9aeb7a6a59fd8aa978f08c8a826
Amir M-Aghaei <amir.maghaei@gmail.com>
2019-08-22 00:51:55 +0200

    Add nnhopping and j1j2 explicit

--- src/hopping.jl
+++ src/hopping.jl
@@ -1,23 +1,58 @@
-function hopping_chain(lx         ::Int64,
-                         t          ::T=1.0,
-                         mu         ::T=0.0;
-                         boundary   ::Symbol=:OBC) where {T<:RLorCX}
+function hopping_chain(lx         ::Int,
+                       t          ::T=1.0,
+                       mu         ::T=0.0;
+                       boundary   ::Symbol=:OBC) where {T<:RLorCX}
 
-hopmatrix = diagm(0 => mu .* ones(T, lx)) +
-    diagm(1 => -t .* ones(T, lx-1)) +
-    diagm(-1 => conj(-t) .* ones(T, lx-1))
+    hopmatrix = diagm(0 => mu .* ones(T, lx)) +
+        diagm(1 => -t .* ones(T, lx-1)) +
+        diagm(-1 => conj(-t) .* ones(T, lx-1))
 
-if boundary == :OBC
-    return hopmatrix
-elseif boundary == :PBC
-    hopmatrix[1, lx] = -t
-    hopmatrix[lx, 1] = conj(-t)
-    return hopmatrix
-elseif boundary == :APBC
-    hopmatrix[1, lx] = t
-    hopmatrix[lx, 1] = conj(t)
-    return hopmatrix
-else
-    error("unrecognized boundary condition : ", boundary)
+    if boundary == :OBC
+        return hopmatrix
+    elseif boundary == :PBC
+        hopmatrix[1, lx] = -t
+        hopmatrix[lx, 1] = conj(-t)
+        return hopmatrix
+    elseif boundary == :APBC
+        hopmatrix[1, lx] = t
+        hopmatrix[lx, 1] = conj(t)
+        return hopmatrix
+    else
+        error("unrecognized boundary condition : ", boundary)
+    end
+    hopmatrix
 end
+
+function nnhoppingchain(lx::Int, t1::T, t2::T, mu::T=0.0;
+                        boundary::Symbol=:OBC) where {T<:RLorCX}
+
+    hopmatrix = diagm(0 => mu .* ones(T, lx)) +
+        diagm(1 => -t1 .* ones(T, lx-1)) +
+        diagm(-1 => conj(-t1) .* ones(T, lx-1)) +
+        diagm(2 => -t2 .* ones(T, lx-1)) +
+        diagm(-2 => conj(-t2) .* ones(T, lx-1))
+
+    if boundary == :OBC
+        return hopmatrix
+    elseif boundary == :PBC
+        hopmatrix[1, lx] = -t1
+        hopmatrix[1, lx-1] = -t2
+        hopmatrix[2, lx] = -t2
+        hopmatrix[lx, 1] = conj(-t1)
+        hopmatrix[lx-1, 1] = conj(-t2)
+        hopmatrix[lx, 2] = conj(-t2)
+        return hopmatrix
+
+    elseif boundary == :APBC
+        hopmatrix[1, lx] = t1
+        hopmatrix[1, lx-1] = t2
+        hopmatrix[2, lx] = t2
+        hopmatrix[lx, 1] = conj(t1)
+        hopmatrix[lx-1, 1] = conj(t2)
+        hopmatrix[lx, 2] = conj(t2)
+        return hopmatrix
+    else
+        error("unrecognized boundary condition : ", boundary)
+    end
+    hopmatrix
 end
