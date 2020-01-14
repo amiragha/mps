28824d358c0024ca60b863205f05cb9176574eb7
Amir M-Aghaei <amir.maghaei@gmail.com>
2020-01-14 16:18:55 +0100

    correct run6leg for Ali

--- run6leg.jl
+++ run6leg.jl
@@ -9,66 +9,82 @@ using ExactDiagonalizationTools
 using SymTensors
 using GaussianFermions
 using GutzwillerMPS
+using TensorNetAlgs
 
 ly = 6
-lx = 24
+lx = 30
 L = lx * ly
 t1, t2, t3, mu = 1.0, 1.0, 1.0, 0.0
-boundaries = [:OBC]
+j, k = 2.0, 0.6
+boundary = (:PBC, :OBC)
+b_str = "PBCY"
 
-for boundary in boundaries
-    println("Statring boundary condition $boundary ...")
-    hamil = generatebdg(
-        triangularhopping((ly, lx), t1, t2, t3, mu, boundary=boundary)
-    )
-    cm = correlationmatrix(hamil, div(L, 2))
-    fgs = generate_fishmangates(cm)
+hamil = generatebdg(
+    triangularhopping((ly, lx), t1, t2, t3, mu, boundary=boundary)
+)
+cm = correlationmatrix(hamil, div(L, 2))
+fgs = corrmat2gmps(cm)
 
-    sz, sp, sm = spinoperators(1/2, symmetry=:U1)
+sz, sp, sm = spinoperators(1/2, symmetry=U1)
 
 
-    infos = "$boundary,ly=$ly,lx=$lx,t1=$t1,t2=$t2,t3=$t3,mu=$mu"
-    for m in [100, 200]
+infos = "$b_str,ly=$ly,lx=$lx,t1=$t1,t2=$t2,t3=$t3,mu=$mu"
+for m in [2000]
 
-        print("Fermionic mps m=$m ")
-        filename = "mps,triangular,hop,$infos,m=$m.jld2"
+    print("Fermionic mps m=$m ")
+    filename = "mps,triangular,hop,$infos,m=$m.jld2"
+    if isfile(filename)
+        println("already exists! loading...")
+        @load filename mps
+    else
+        println("making ...")
+        mps = gmps2mps(fgs, m, symmetry=U1)
+        #@save filename mps
+
+        # zzdata = measure(mps, sz, sz)
+        # pmdata = measure(mps, sp, sm)
+        vnedata = entanglemententropy(mps)
+
+        println("Measurements for hop m = $m ...")
+        # writedlm( "zz,triangular,hop,$infos,m=$m.dat", zzdata)
+        # writedlm( "pm,triangular,hop,$infos,m=$m.dat", pmdata)
+        writedlm( "vne,triangular,hop,$infos,m=$m.dat", vnedata)
+    end
+
+    for M in [12000]
+        println("ZipandGutzwiller for M = $M ...")
+
+        #filename = "mps,triangular,gutzF23,$infos,m=$m,M=$M.jld2"
         if isfile(filename)
-            println("already exists! loading...")
-            @load filename mps
+            println("Already exists! dismiss.")
         else
-            println("making ...")
-            mps = fishman2mps(fgs, m, symmetry=:U1)
-            @save filename mps
+            println("Making ...")
+            mpsgutz = zipandgutzwiller!(mps, mps, mode=:F23, maxdim=M)
+            normalize!(mpsgutz)
+            #@save  filename mpsgutz
+            println("Measurements for M = $M ...")
+            # zzdata = measure(mpsgutz, sz, sz)
+            # pmdata = measure(mpsgutz, sp, sm)
+            vnedata = entanglemententropy(mpsgutz)
+            # writedlm("zz,triangular,gutzF23,$infos,m=$m,M=$M.dat", zzdata)
+            # writedlm("pm,triangular,gutzF23,$infos,m=$m,M=$M.dat", pmdata)
+            writedlm("vne,triangular,gutzF23,$infos,m=$m,M=$M.dat", vnedata)
 
-            zzdata = real.(measure_2point(mps, sz, sz))
-            pmdata = real.(measure_2point(mps, sp, sm))
-            vnedata = entanglemententropy(mps)
+            mpo = generatempo(triangularspinmodel((ly, lx), j, j, j, k, k, k,
+                                                  boundary=boundary, symmetry=U1))
+            reducempo!(mpo)
+            center_at!(mpsgutz, 1)
+            env = initialenv(mps, mpo)
+            dmrg2sitesweep!(mps, mpo, env, maxdim=M, verbose=true)
 
-            println("Measurements for hop m = $m ...")
-            writedlm( "zz,triangular,hop,$infos,m=$m.dat", zzdata)
-            writedlm( "pm,triangular,hop,$infos,m=$m.dat", pmdata)
-            writedlm( "vne,triangular,hop,$infos,m=$m.dat", vnedata)
-        end
+            dmrginfo = "j=$j,k=$k,M=$M"
+            # zzdata = measure(mpsgutz, sz, sz)
+            # pmdata = measure(mpsgutz, sp, sm)
+            vnedata = entanglemententropy(mpsgutz)
+            # writedlm("zz,triangular,1dmrg,$dmrginfo,gutzF23,$infos,m=$m,M=$M.dat", zzdata)
+            # writedlm("pm,triangular,1dmrg,$dmrginfo,gutzF23,$infos,m=$m,M=$M.dat", pmdata)
+            writedlm("vne,triangular,1dmrg,$dmrginfo,gutzF23,$infos,m=$m,M=$M.dat", vnedata)
 
-        for M in [600, 900]
-            println("ZipandGutzwiller for M = $M ...")
-
-            filename = "mps,triangular,gutzF23,$infos,m=$m,M=$M.jld2"
-            if isfile(filename)
-                println("Already exists! dismiss.")
-            else
-                println("Making ...")
-                mpsgutz = zipandgutzwiller!(mps, mps, mode=:F23, maxdim=M)
-                normalize!(mpsgutz)
-                @save  filename mpsgutz
-                println("Measurements for M = $M ...")
-                zzdata = real.(measure_2point(mpsgutz, sz, sz))
-                pmdata = real.(measure_2point(mpsgutz, sp, sm))
-                vnedata = entanglemententropy(mpsgutz)
-                writedlm("zz,triangular,gutzF23,$infos,m=$m,M=$M.dat", zzdata)
-                writedlm("pm,triangular,gutzF23,$infos,m=$m,M=$M.dat", pmdata)
-                writedlm("vne,triangular,gutzF23,$infos,m=$m,M=$M.dat", vnedata)
-            end
         end
     end
 end
